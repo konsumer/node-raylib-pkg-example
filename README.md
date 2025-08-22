134a169d6baf08482b1ea9fc028d2448736af6a4
David Konsumer <konsumer@jetboystudio.com>
2025-08-21 18:49:46 -0700

    working pkg

--- README.md
+++
@@ -1,1 +0,0 @@
-# node-raylib-pkg-example
---
+++ test.js
@@ -0,0 +1,13 @@
+const r = require('raylib')
+
+r.InitWindow(800, 450, 'raylib [core] example - basic window')
+
+r.SetTargetFPS(60)
+while (!r.WindowShouldClose()) {
+  r.BeginDrawing()
+  r.ClearBackground(r.RAYWHITE)
+  r.DrawText('Congrats! You created your first node-raylib window!', 120, 200, 20, r.LIGHTGRAY)
+  r.EndDrawing()
+}
+
+r.CloseWindow()
